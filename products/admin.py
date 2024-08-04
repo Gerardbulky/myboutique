3e70f27fc9c91bb52a1dfdc91f80bdf228047db9
Gerardbulky <gerardambe@yahoo.com>
2024-08-04 22:38:46 +0200

    Added products views and templates

--- products/admin.py
+++ products/admin.py
@@ -3,25 +3,25 @@ from .models import Product, Category
 # Register your models here.
 
 
-# class ProductAdmin(admin.ModelAdmin):
-#     list_display = (
-#         'sku',
-#         'name',
-#         'category',
-#         'price',
-#         'rating',
-#         'image',
-#     )
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'name',
+        'category',
+        'price',
+        'rating',
+        'image',
+    )
 
-#     ordering = ('sku',)
-
-
-# class CategoryAdmin(admin.ModelAdmin):
-#     list_display = (
-#         'friendly_name',
-#         'name',
-#     )
+    ordering = ('sku',)
 
 
-admin.site.register(Product)
-admin.site.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
